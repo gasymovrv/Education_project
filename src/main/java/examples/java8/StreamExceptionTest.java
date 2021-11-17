b74f08d59aeff426cf3213b7b0b68815a66ac2fb
Ruslan Gasymov <RGasymov@luxoft.com>
2021-11-17 16:35:56 +0400

    Add examples of catching exception in streams

---
+++ src/main/java/examples/java8/StreamExceptionTest.java
@@ -0,0 +1,116 @@
+package examples.java8;
+
+import io.vavr.control.Try;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamExceptionTest {
+    public static void main(String[] args) {
+        testConsume();
+        testVavr();
+        testRethrow();
+    }
+
+    private static void testRethrow() {
+        List<String> strings = Stream.of(new Person("1"))
+                .filter(p -> p.getName().equals("1"))
+                .map(person -> tryOrElseRethrow(StreamExceptionTest::canFail, person))
+                .collect(Collectors.toList());
+        System.out.println(strings);
+    }
+
+    private static void testConsume() {
+        List<String> strings = Stream.of(new Person("1"))
+                .filter(p -> p.getName().equals("1"))
+                .map(person -> tryGet(() -> canFail(person)).orElse("default"))
+                .collect(Collectors.toList());
+        System.out.println(strings);
+    }
+
+    private static void testVavr() {
+        List<String> strings = Stream.of(new Person("1"))
+                .filter(p -> p.getName().equals("1"))
+                .map(person -> Try
+                        .of(() -> canFail(person))
+                        .onFailure(e -> System.out.println("Vavr.Try, " + e.getMessage()))//log error
+                        .getOrElse("default"))
+                .collect(Collectors.toList());
+        System.out.println(strings);
+    }
+
+    private static String canFail(Person p) throws Exception {
+        throw new IOException("oops");
+    }
+
+    public static <T, R> R tryOrElseRethrow(CheckedFunction<T, R> checkedFunction, T arg) {
+        try {
+            return checkedFunction.apply(arg);
+        } catch (Exception e) {
+            System.out.println("tryOrElseRethrow, " + e.getMessage());//log error
+            throw new RuntimeException(e);//custom unchecked exception
+        }
+    }
+
+    public static <R> R tryOrElseRethrow(CheckedSupplier<R> checkedSupplier) {
+        try {
+            return checkedSupplier.get();
+        } catch (Exception e) {
+            System.out.println("tryOrElseRethrow, " + e.getMessage());//log error
+            throw new RuntimeException(e);//custom unchecked exception
+        }
+    }
+
+    public static <R> Optional<R> tryGet(CheckedSupplier<R> checkedSupplier) {
+        try {
+            return Optional.ofNullable(checkedSupplier.get());
+        } catch (Exception e) {
+            System.out.println("tryGet, " + e.getMessage());//log error
+            return Optional.empty();
+        }
+    }
+
+    public static void tryDo(Runnable doFunc) {
+        try {
+            doFunc.run();
+        } catch (Exception e) {
+            System.out.println("tryDo, " + e.getMessage());//log error
+        }
+    }
+
+    public static <T, R> Function<T, R> wrapToUnchecked(CheckedFunction<T, R> checkedFunction) {
+        return t -> {
+            try {
+                return checkedFunction.apply(t);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());//log error
+                throw new RuntimeException(e);
+            }
+        };
+    }
+
+    @FunctionalInterface
+    private interface CheckedFunction<T, R> {
+        R apply(T t) throws Exception;
+    }
+
+    @FunctionalInterface
+    private interface CheckedSupplier<R> {
+        R get() throws Exception;
+    }
+
+    private static class Person {
+        private final String name;
+
+        public Person(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+}
