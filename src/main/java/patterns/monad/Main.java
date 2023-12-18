ec34556d319d67a102ede642109ffee3dc53e273
Ruslan Gasymov <RGasymov@luxoft.com>
2023-12-19 00:14:09 +0400

    Add monad and additional info about N+1

---
+++ src/main/java/patterns/monad/Main.java
@@ -0,0 +1,20 @@
+package patterns.monad;
+
+public class Main {
+
+  public static void main(String[] args) {
+    var obj = new TestClass("name 1");
+    Monad.from(obj)
+        .map(TestClass::name)
+        .map(it -> it.charAt(0))
+        .map(it -> {
+          System.out.println(it);
+          return it;
+        });
+  }
+
+  record TestClass(String name) {
+
+  }
+}
+
---
+++ src/main/java/patterns/monad/Monad.java
@@ -0,0 +1,20 @@
+package patterns.monad;
+
+import java.util.function.Function;
+
+public class Monad<T> {
+
+  private T value;
+
+  private Monad(T value) {
+    this.value = value;
+  }
+
+  public static <T> Monad<T> from(T value) {
+    return new Monad<>(value);
+  }
+
+  public <R> Monad<R> map(Function<T, R> func) {
+    return new Monad<>(func.apply(value));
+  }
+}
