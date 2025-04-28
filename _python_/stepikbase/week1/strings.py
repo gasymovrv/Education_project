57ea40679c8662639aeda78d40346dbc754be400
Ruslan Gasymov <RGasymov@luxoft.com>
2025-04-28 17:59:17 +0400

    Add examples of Python string

--- _python_/stepikbase/week1/strings.py
+++ _python_/stepikbase/week1/strings.py
@@ -62,3 +62,65 @@ print(is_signed_number("-12.3"))  # True
 print(is_signed_number("Ⅻ"))  # False
 print(is_signed_number("½"))  # False
 print(is_signed_number("²"))  # False
+
+genome = "acggtgttat"
+
+cCount = genome.upper().count("C")
+gCount = genome.upper().count("G")
+percent = len(genome) / 100
+
+print((gCount + cCount) / percent)
+
+print("================ slicing ================")
+dna = "ATTCGGAGCT"
+print("dna=", dna)
+print("dna[1]=", dna[1])  # char 1
+print("dna[1:4]=", dna[1:4])  # from 1 to 4
+print("dna[:4]=", dna[:4])  # from 0 to 4
+print("dna[4:]=", dna[4:])  # from 4 to the end
+print("dna[1:-1:2]=", dna[1:-1:2])  # from 1 to -1 with step 2
+print("dna[::-1]=", dna[::-1])  # reverse (step -1)
+print("dna[-100:100]=", dna[-100:100])  # no error, just whole string
+
+
+def split_every_n_chars(s: str, n: int) -> list[str]:
+    """Splits a string into parts of n characters"""
+    return [s[i:i+n] for i in range(0, len(s), n)]
+
+
+def insert_every_n_chars(s: str, n: int, sep: str = "-") -> str:
+    """Splits a string into parts of n characters, inserts a separator between them and joins them"""
+    return sep.join(s[i:i+n] for i in range(0, len(s), n))
+
+
+def insert_every_n_chars_from_end(s: str, n: int, sep: str = "-") -> str:
+    """
+    Backward version of insert_every_n_chars
+    Need when we need first part with float length
+    """
+    parts = []
+    i = len(s)
+    while i > 0:
+        parts.append(s[max(0, i-n):i])
+        i -= n
+    return sep.join(reversed(parts))
+
+
+s = "0123456789"
+print(split_every_n_chars(s, 3))
+print(insert_every_n_chars(s, 3))
+print(insert_every_n_chars(s, 3, "#"))
+
+
+def reverse_str(s: str, k: int) -> str:
+    result = list(s)
+
+    for i in range(0, len(s), 2 * k):
+        # Reverse first k characters in each 2k block by slice assignment.
+        # 'reversed' returns an iterator, but slice accepts any iterable.
+        result[i:i+k] = reversed(result[i:i+k])
+
+    return "".join(result)
+
+
+print(reverse_str("abcdefg", 2))
--- _python_/stepikmiddle/chapter2/functools_and_lambda/iterating_and_lambda.py
+++ _python_/stepikmiddle/chapter2/functools_and_lambda/iterating_and_lambda.py
@@ -2,17 +2,21 @@ import random
 from functools import cmp_to_key, lru_cache
 
 # x = input("Provide 2 numbers:\n").split()
-# print(x)  # ["1", "2"]
+x = ["1", "2"]
+print(x)  # ["1", "2"]
 
 # first argument of map is function (constructor of type int is also function int.__init__(str) -> int),
 # second argument is iterable.
 # n, k - unpacking of iterable object. if iterable is more or less than 2 elements, it throws error
-# n, k = map(int, x)
-# print(n + k)
+n, k = map(int, x)
+print(n + k)
+
+x2 = [1, 2, 3, 4, 5]
+print(min(map(lambda item: abs(item - 1), x2)))
 
 # the same as `n, k = map(int, x)` but using generator instead
-# n, k = (int(i) for i in x)
-# print(n + k)
+n, k = (int(i) for i in x)
+print(n + k)
 
 # xs = (int(i) for i in input("Provide numbers for filter by even:\n").split())
 # iterator = filter(lambda num: num % 2 == 0, xs)
